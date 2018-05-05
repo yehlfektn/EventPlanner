0385815cc7dbf0ab90d1279b86471f7b2c297efb
yehlfektn <nurdaulet.kenges@nu.edu.kz>
2018-05-05 11:12:02 +0600

    library update

--- app/src/main/java/gravityfalls/eventplanner/MainActivity.java
+++ app/src/main/java/gravityfalls/eventplanner/MainActivity.java
@@ -2,12 +2,52 @@ package gravityfalls.eventplanner;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.DividerDrawerItem;
+import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private Drawer result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initToolbar();
+        initDrawer();
+    }
+
+    private void initToolbar() {
+        toolbar =  findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        setTitle("");
+    }
+
+    private void initDrawer() {
+        result = new DrawerBuilder()
+                .withActivity(this)
+                .withToolbar(toolbar)
+                .addDrawerItems(
+                        new DividerDrawerItem(),
+                        new SecondaryDrawerItem().withName("Hello World"),
+                        new SecondaryDrawerItem().withName("Hello World 2")
+                )
+                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                    @Override
+                    public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
+                        // do something with the clicked item :D
+                        result.setSelection(position);
+                        return false;
+                    }
+                })
+                .build();
     }
 }
